a65adb15e7a5ca770e5de147f8a04d2d827e5157
Gadnalf <philip.cai.mao@hotmail.ca>
2022-03-15 19:31:06 -0400

    Removed stuff from main

--- main.cpp
+++ main.cpp
@@ -1,29 +1,10 @@
-#include "EthernetNetIf.h"
-#include "NTPClient.h"
 #include "mbed.h"
-
-EthernetNetIf eth; 
-NTPClient ntp;
 
 // main() runs in its own thread in the OS
 int main() {
   printf("Start\n");
 
   printf("Setting up...\n");
-  EthernetErr ethErr = eth.setup();
-  if (ethErr) {
-    printf("Error %d in setup.\n", ethErr);
-    return -1;
-  }
+
   printf("Setup OK\r\n");
-
-  time_t ctTime;
-  ctTime = time(NULL);
-  printf("Current time is (UTC): %s\n", ctime(&ctTime));
-
-  Host server(IpAddr(), 123, "0.uk.pool.ntp.org");
-  ntp.setTime(server);
-
-  ctTime = time(NULL);
-  printf("\nTime is now (UTC): %s\n", ctime(&ctTime));
 }
