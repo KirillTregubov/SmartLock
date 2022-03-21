7e43a0d6121ef9aa0d15ad34dff28a1fd6080fe3
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-03-20 21:56:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
-/* WiFi Example
- * Copyright (c) 2018 ARM Limited
+/* SmartLock
+ * Copyright (c) 2022 Kirill Tregubov & Philip Cai
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Software is distributed and provided "AS IS", WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
  */
  
 #include "mbed.h"
@@ -84,8 +75,7 @@ void set_rtc() {
         set_time(timestamp);
     }
 }
-
-static void generate_private_key(void)
+void generate_private_key()
 {
     uint8_t random[20] = { 0 };
 
