d894d39ac6b4a2369dd785c3a4c9b66756415484
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-03-23 02:47:39 -0400

    Cleaned up code, added factory default RTC time

--- main.cpp
+++ main.cpp
@@ -1,81 +1,91 @@
-/* SmartLock
- * Copyright (c) 2022 Kirill Tregubov & Philip Cai
+/**
+ * @file main.cpp
+ * @author Kirill Tregubov (KirillTregubov)
+ * @author Philip Cai (Gadnalf)
+ * @copyright Copyright (c) 2022 Kirill Tregubov & Philip Cai
+ * @brief Main SmartLock program.
  *
- * Software is distributed and provided "AS IS", WITHOUT WARRANTIES
- * OR CONDITIONS OF ANY KIND, either express or implied.
+ * This generates the private key, syncs the RTC with an NTP
+ * server, and initializes the BLE.
+ *
+ * @bug No known bugs.
  */
 
-#include "TCPSocket.h"
 #include "mbed.h"
-#include "mbedtls/version.h" // unsure if needed
 #include "ntp-client/NTPClient.h"
-#include "psa/crypto.h"
 #include <ctype.h>
+// #include "TCPSocket.h"
+// #include <time.h>
+// #include "mbedtls/version.h"
+// #include "psa/crypto.h"
 
-// #define WIFI_IDW0XX1    2
-#if (defined(TARGET_DISCO_L475VG_IOT01A))
+#if defined(TARGET_DISCO_L475VG_IOT01A)
 #include "ISM43362Interface.h"
 ISM43362Interface wifi(false);
 #endif
 
-const char *sec2str(nsapi_security_t sec) {
-  switch (sec) {
-  case NSAPI_SECURITY_NONE:
-    return "None";
-  case NSAPI_SECURITY_WEP:
-    return "WEP";
-  case NSAPI_SECURITY_WPA:
-    return "WPA";
-  case NSAPI_SECURITY_WPA2:
-    return "WPA2";
-  case NSAPI_SECURITY_WPA_WPA2:
-    return "WPA/WPA2";
-  case NSAPI_SECURITY_UNKNOWN:
-  default:
-    return "Unknown";
-  }
+/**
+ * @brief Synchronizes the RTC to a hardcoded epoch.
+ *
+ * @return Void.
+ */
+void sync_rtc_with_factory() {
+  set_time(1648016868);
+  time_t factory_time = time(NULL);
+  printf("> Defaulted RTC to %s.", ctime(&factory_time));
 }
 
-int scan_demo(WiFiInterface *wifi) {
-  WiFiAccessPoint *ap;
-
-  printf("> Scan WiFi\n");
-
-  return wifi->scan(NULL, 0);
-}
-
-void set_rtc() {
+/**
+ * @brief Synchronizes the RTC to the epoch returned by
+ *        the default NTP server.
+ *
+ * The default NTP server is 2.pool.ntp.org port 123.
+ *
+ * @return Void.
+ */
+void sync_rtc_with_ntp() {
   NTPClient ntp(&wifi);
-  // default server: 2.pool.ntp.org and default port: 123
   time_t timestamp = ntp.get_timestamp();
   if (timestamp < 0) {
     printf("An error occurred when getting the time. (code %u)\n", timestamp);
+    sync_rtc_with_factory();
   } else {
-    printf("> Set RTC to %s", ctime(&timestamp));
+    printf("> Synced RTC to %s", ctime(&timestamp));
     set_time(timestamp);
   }
 }
 
+/**
+ * @brief Convert a string to uppercase.
+ *
+ * @param str The string that you want to convert to uppercase.
+ * @return The address of the string.
+ */
 char *strupr(char *str) {
-  unsigned char *p = (unsigned char *)str;
+  unsigned char *temp = (unsigned char *)str;
 
   // Convert to upper case
-  while (*p) {
-    *p = toupper((unsigned char)*p);
-    p++;
+  while (*temp) {
+    *temp = toupper((unsigned char)*temp);
+    temp++;
   }
 
   return str;
 }
 
-void generate_private_key() {
+/**
+ * @brief Generate a private key using onboard TRNG chip.
+ *
+ * @return The address of the generated key.
+ */
+char *generate_private_key() {
   printf("> Generate key\n");
 
   psa_status_t status;
   status = psa_crypto_init();
   if (status != PSA_SUCCESS) {
     printf("Failed to initialize PSA Crypto\n");
-    return;
+    return NULL;
   }
 
   size_t exported_length = 0;
@@ -91,13 +101,13 @@ void generate_private_key() {
   status = psa_generate_key(&attributes, &id);
   if (status != PSA_SUCCESS) {
     printf("Failed to generate key (%d)\n", status);
-    return;
+    return NULL;
   }
 
   status = psa_export_key(id, exported, sizeof(exported), &exported_length);
   if (status != PSA_SUCCESS || exported_length != 10) {
     printf("Failed to export key (%d)\n", status);
-    return;
+    return NULL;
   }
 
   char key[exported_length];
@@ -107,32 +117,51 @@ void generate_private_key() {
   }
   printf("Key: %s (len: %d)\n", strupr(key), strlen(key));
 
-  /* Clean up */
   psa_reset_key_attributes(&attributes);
   mbedtls_psa_crypto_free();
+
+  return strupr(key);
 }
 
-int main() {
-  printf("=== SmartLock booted ===\n");
-  generate_private_key();
-
-  int count = scan_demo(&wifi);
-  if (count == 0) {
+/**
+ * @brief Connect to the WiFi network specified in 'mbed_app.json'.
+ *
+ * @return 0 upon success, -1 on error / failure.
+ */
+int connect_to_wifi() {
+  WiFiAccessPoint *ap;
+  printf("> Searching for WiFi networks\n");
+  int network_count = wifi.scan(NULL, 0);
+  if (network_count <= 0) {
     printf("No WiFi hotspots found - can't continue further.\n");
     return -1;
   }
 
-  printf("> Connect to %s...\n", MBED_CONF_APP_WIFI_SSID);
-  int ret = wifi.connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD,
+  printf("> Connecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
+  int status = wifi.connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD,
                          NSAPI_SECURITY_WPA_WPA2);
-  if (ret != 0) {
+  if (status != 0) {
     printf("Connection error\n");
     return -1;
   }
 
   printf("> Connection success! IP: %s\n", wifi.get_ip_address());
-  set_rtc();
+  return 0;
+}
 
+int main() {
+  printf("=== SmartLock booted ===\n");
+  char *key = generate_private_key();
+  printf("Generated key %s", key);
+
+  int status = connect_to_wifi();
+  if (status < 0) {
+    sync_rtc_with_factory();
+  } else {
+    sync_rtc_with_ntp();
+  }
   wifi.disconnect();
+
+
   printf("Terminated\n");
 }
