99d5b295f9a3cd2ec2cd0b52b124d7b1e31991e3
Philip <philip.cai.mao@hotmail.ca>
2022-03-24 20:11:10 -0400

    Header guard

--- datastore.h
+++ datastore.h
@@ -1,4 +1,6 @@
 // Header file for datastore.
+#ifndef DATASTORE
+#define DATASTORE
 
 int get_private_key(char* buf);
 
@@ -7,3 +9,5 @@ int get_reset_key(char* buf);
 int set_private_key(const char* key);
 
 int set_reset_key(const char* key);
+
+#endif
